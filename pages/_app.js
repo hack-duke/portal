0c664dacefa5fc156c614af19bd26e1f3715893a
Yihong Song <ys377@duke.edu>
2023-04-03 22:48:52 -0400

    Reinit Next Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,5 @@
-import "../styles/globals.css";
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "../node_modules/nprogress/nprogress.css"; //styles of nprogress
-import React, { useEffect } from "react";
-//Binding events.
+import '@/styles/globals.css'
 
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
